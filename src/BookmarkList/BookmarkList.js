452a71f59b734e25f0a4583a1172a708add9e6b0
Kevin Hoercher <khoerch@gmail.com>
2019-08-21 15:27:40 -0400

    proptypes

--- src/BookmarkList/BookmarkList.js
+++ src/BookmarkList/BookmarkList.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import BookmarkItem from '../BookmarkItem/BookmarkItem';
+import PropTypes from 'prop-types'
+import BookmarksContext from '../BookmarksContext'
 import './BookmarkList.css'
 
 class BookmarkList extends Component {
-  static defaultProps = {
-    bookmarks: []
-  };
+  static contextType = BookmarksContext
 
   render() {
-    const { bookmarks } = this.props
+    const { bookmarks } = this.context
     return (
       <section className='BookmarkList'>
         <h2>Your bookmarks</h2>
@@ -23,6 +23,15 @@ class BookmarkList extends Component {
       </section>
     );
   }
+}
+
+BookmarkList.propTypes = {
+  bookmarks: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    rating: PropTypes.number,
+    description: PropTypes.string
+  }))
 }
 
 export default BookmarkList;
